1cd95995fd515c01197d115502e0d9042b0c5308
atech-01 <developeratech01@gmail.com>
2025-04-09 20:30:30 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ticket_apps/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ticket_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/cupertino.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  final List appScreen = [
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile"),
+  ];
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text('My ticket'))),
+      body: Center(child: appScreen[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            activeIcon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            activeIcon: Icon(Icons.search_outlined),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.airplane_ticket),
+            activeIcon: Icon(Icons.airplane_ticket_outlined),
+            label: 'Ticket',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            activeIcon: Icon(Icons.person_outlined),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
