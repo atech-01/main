9ba48e28f09f0a03e8f60c8fd867af6722d67bfe
atech-01 <developeratech01@gmail.com>
2025-04-10 07:45:18 +0100

    External Styles

---
+++ lib/base/res/styles/app_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static TextStyle headLineStyle1 = const TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle headLineStyle3 = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w500,
+  );
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// import 'app_styles.dart';
+import 'package:ticket_apps/base/res/styles/app_styles.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,24 +10,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String greeting() {
+    var hour = DateTime.now().hour;
+    if (hour < 12) {
+      return "Good Morning";
+    } else if (hour < 17) {
+      return "Good Afternoon";
+    } else {
+      return "Good Evening";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: [
-          Column(
-            children: [
-              Row(
-                children: [
-                  Column(children: [Text("Text 1"), Text("Text 2")]),
-                  Container(),
-                ],
-              ),
-              Row(children: [
-                  
-                ],
-              ),
-            ],
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(greeting(), style: AppStyles.headLineStyle3),
+                        SizedBox(height: 5),
+                        Text("Book Tickets", style: AppStyles.headLineStyle1),
+                      ],
+                    ),
+                    Container(color: Colors.red, height: 100, width: 100),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Search Icon"), Text("Empty Space")],
+                ),
+              ],
+            ),
           ),
         ],
       ),
